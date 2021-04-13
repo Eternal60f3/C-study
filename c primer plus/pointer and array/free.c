cf516c6ba39165790a369b9142a4e30cc0b86fe8
Eternal <2036019675@qq.com>
2021-04-13 18:05:16 +0800

    c

--- c primer plus/pointer and array/free.c
+++ c primer plus/pointer and array/free.c
@@ -3,6 +3,8 @@ void main(void)
 {
     int * pointer;
     int num[3] = {2, 2, 3};
-    printf("%d", num[0] + 1);
+    pointer = num;
+    printf("%d\n", num[0] + 1);
+    printf("%p", &pointer);
     
 }
