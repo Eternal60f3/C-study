20800a30423061e80b41ea0d1b50884ee9149645
Eternal <2036019675@qq.com>
2021-04-18 13:41:04 +0800

    7-1

--- mooc/tempCodeRunnerFile.c
+++ mooc/tempCodeRunnerFile.c
@@ -1,33 +1,89 @@
 #include <stdio.h>
-#define MAX 100
-#define MIN 10
-
+#include <stdbool.h>
 int main(void)
 {
-    int nums[200];
-    int small = 0, big = 0;
-    int scale = 10;
-    printf("enter two num please such 16/19\n");
-    while (scanf("%d/%d", &small, &big) != 2 || small >= big || small >= MAX || big < MIN)
+    int coe; // 系数
+    int power;
+    int coes[1000];
+    int powers[1000];
+    int coe_index1 = 0;
+    int power_index1 = 0;
+
+    for (; scanf("%d %d", &power, &coe) == 2; coe_index1++, power_index1++)
     {
-        printf("two nums small/big\n");
+        if (coe == 0)
+            continue;
+        powers[power_index1] = power;
+        coes[coe_index1] = coe;
     }
 
-    int index = 0;
-    for (; index < 200; index++)
+    int p_index2 = 0, c_index2 = 0;
+    int same_power[1000];
+    int s_index = 0;
+    int power_coe[100][2];
+
+    int index_same[100];
+    int index_same_index = 0;
+    for (; p_index2 < power_index1; p_index2++)
     {
-        nums[index] = (small * 10) / big;
-        small = (small * 10) % big;
-        if (small == 0)
+        bool is_not_same = true;
+        int s_index1 = 0;
+        for (s_index1 = 0; s_index1 < s_index; s_index1++)
+        {
+            if (power_coe[s_index1][0] == powers[p_index2])
+            {
+                is_not_same = false;
+                power_coe[s_index1][1] += coes[p_index2];
+            }
+        }
+        if (is_not_same)
+        {
+            power_coe[s_index][0] = powers[p_index2];
+            power_coe[s_index][1] = coes[p_index2];
+            s_index++;
+            index_same_index++;
+        }
+    }
+    int o = 0;
+    int o1 = 0;
+    for (o = 0; o < s_index - 1; o++)
+    {
+        bool issorted = true;
+        for (o1 = 0; o1 < s_index - 1 - o; o1++)
+        {
+            int temp = 0;
+            if (power_coe[o1][0] < power_coe[o1 + 1][0])
+            {
+                int p = 0;
+                for (p = 0; p < 2; p++)
+                {
+                    temp = power_coe[o1][p];
+                    power_coe[o1][p] = power_coe[o1 + 1][p];
+                    power_coe[o1 + 1][p] = temp;
+                }
+                issorted = false;
+            }
+        }
+        if (issorted)
             break;
     }
 
-    printf("0.");
     int i = 0;
-    for (; i < index; i++)
+    for (i = 0; i < s_index; i++)
     {
-        printf("%d", nums[i]);
+        if (power_coe[i][0] == 0)
+            printf("+%d", power_coe[i][1]);
+        else if (power_coe[i][0] == 1)
+        {
+            printf("+%dx", power_coe[i][1]);
+        }
+        else if (power_coe[i][1] == 1)
+        {
+            printf("x%d", power_coe[i][0]);
+        }
+        else
+            printf("+%dx%d", power_coe[i][1], power_coe[i][0]);
+
     }
-    printf("\n");
     return 0;
 }
