5f584bc78729a47d1686af19b2ce8611ec640271
Eternal <2036019675@qq.com>
2021-04-18 21:54:01 +0800

    debug

--- mooc/7-1.c
+++ mooc/7-1.c
@@ -2,5 +2,119 @@
 #include <stdbool.h>
 int main(void)
 {
-    
+    int coe; // 系数
+    int power;
+    int coes[1000];
+    int powers[1000];
+    int coe_index1 = 0;
+    int power_index1 = 0;
+
+    for (; scanf("%d %d", &power, &coe) == 2;)
+    {
+        if (coe == 0)
+        {
+            continue;
+        }
+        else
+        {
+            powers[power_index1] = power;
+            coes[coe_index1] = coe;
+            coe_index1++, power_index1++;
+        }
+    }
+
+    // int l = 0;
+    // for (; l < coe_index1; l++)
+    // {
+    //     printf("%d\n", coes[l]);
+    // }
+    // printf("i");
+    int p_index2 = 0;
+    int s_index = 0;
+    int power_coe[100][2];
+
+    for (; p_index2 < power_index1; p_index2++)
+    {
+        bool is_not_same = true;
+        int s_index1 = 0;
+
+        for (; s_index1 < s_index; s_index1++)
+        {
+            if (power_coe[s_index1][0] == powers[p_index2])
+            {
+                is_not_same = false;
+                power_coe[s_index1][1] += coes[p_index2];
+            }
+        }
+        if (is_not_same)
+        {
+            power_coe[s_index][0] = powers[p_index2];
+            power_coe[s_index][1] = coes[p_index2];
+            s_index++;
+            // printf("%dx%d\n", power_coe[0][1], power_coe[0][0]);
+        }
+    }
+    // printf("%d\n", power_coe[0][1]);
+    int o = 0;
+    int o1 = 0;
+    for (o = 0; o < s_index - 1; o++)
+    {
+        bool issorted = true;
+        for (o1 = 0; o1 < s_index - 1 - o; o1++)
+        {
+            int temp = 0;
+            if (power_coe[o1][0] < power_coe[o1 + 1][0])
+            {
+                int p = 0;
+                for (p = 0; p < 2; p++)
+                {
+                    temp = power_coe[o1][p];
+                    power_coe[o1][p] = power_coe[o1 + 1][p];
+                    power_coe[o1 + 1][p] = temp;
+                }
+                issorted = false;
+            }
+        }
+        if (issorted)
+            break;
+    }
+    // printf("%d\n", s_index);
+    // printf("q-%d\n", power_coe[i][1]);
+    int i = 0;
+    for (i = 0; i < s_index; i++)
+    {
+        if (i == 0)
+        {
+            if (power_coe[i][0] == 0)
+            {
+                printf("%d", power_coe[i][1]);
+            }
+            else if (power_coe[i][0] == 1)
+            {
+                printf("%dx", power_coe[i][1]);
+            }
+            else if (power_coe[i][1] == 1)
+            {
+                printf("x%d", power_coe[i][0]);
+            }
+            else
+                printf("%dx%d", power_coe[i][1], power_coe[i][0]);
+        }
+        else
+        {
+            if (power_coe[i][0] == 0)
+                printf("+%d", power_coe[i][1]);
+            else if (power_coe[i][0] == 1)
+            {
+                printf("+%dx", power_coe[i][1]);
+            }
+            else if (power_coe[i][1] == 1)
+            {
+                printf("x%d", power_coe[i][0]);
+            }
+            else
+                printf("+%dx%d", power_coe[i][1], power_coe[i][0]);
+        }
+    }
+    return 0;
 }
