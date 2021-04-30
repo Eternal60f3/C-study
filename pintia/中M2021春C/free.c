9f8d73e6a62e36b93679bb982e91dfeef72ced36
Eternal <2036019675@qq.com>
2021-04-30 11:52:41 +0800

    free更改

---
+++ pintia/中M2021春C/free.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define LIMIT 24 * 60
+void main(void)
+{
+    int time = 0;         // 输入的时间
+    int total_minute = 0; // 输入的时间转为分钟后
+    int add_minute = 0;   // 需要添加的时间
+    int after_add = 0;    // total_minute加上需要添加的分钟数
+
+    scanf("%d %d", &time, &add_minute);
+    int hour = time / 100;
+    int minute = time - hour * 100;
+    total_minute = hour * 60 + minute;
+
+    after_add = total_minute + add_minute;
+    int hour_after_add = after_add / 60;
+    int minute_after_add = after_add % 60;
+    if (hour_after_add >= 24)
+    {
+        hour_after_add = 0 + (hour_after_add - 12);
+    }
+    printf("%d%02d", hour_after_add, minute_after_add);
+}
