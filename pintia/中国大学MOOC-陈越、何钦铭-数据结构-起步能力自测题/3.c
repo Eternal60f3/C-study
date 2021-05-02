966b4bbd7c6137a1db776185d8557916fc388225
Eternal <2036019675@qq.com>
2021-05-02 09:42:10 +0800

    3

--- pintia/中国大学MOOC-陈越、何钦铭-数据结构-起步能力自测题/3.c
+++ pintia/中国大学MOOC-陈越、何钦铭-数据结构-起步能力自测题/3.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 int main(void)
+
 {
-    int num_list[100]; // 整数数组
     int amount = 0;    // 整数的个数
     int move = 0;      // 移动的位数
-    return 0;
+
+    scanf("%d %d", &amount, &move);
+    int num_list[amount]; // 整数数组
+
+    int num_index = 0;
+    for (;num_index < amount;num_index++)
+    {
+        scanf("%d", &num_list[num_index]);
+    }
+
+    for (num_index = 0; num_index < amount; num_index++)
+    {
+        int temporary = 0;
+    }   
 }
