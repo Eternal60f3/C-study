fd8abc002883a602e4422e9a5964287326a71258
Eternal <2036019675@qq.com>
2021-06-20 12:01:10 +0800

    12章-掷骰子练习

---
+++ c primer plus/12.存储类别/掷骰子/用户界面.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "head.h"
+
+void main(void)
+{
+    int sides, once_count, total;
+    int status;
+
+    printf("enter dice_sides\n");
+    while (scanf("%d", &sides) == 1 && sides > 2)
+    {
+        printf("enter once_count\n");
+        if (status = scanf("%d", &once_count) != 1 && once_count < 1)
+        {
+            if (status == EOF)
+            {
+                break;
+            }
+            else
+            {
+                printf("enter int num please\n");
+                while (getchar() != '\n')
+                {
+                    continue;
+                }
+                printf("enter dice_sides\n");
+                continue;
+            }
+        }
+        total = roll_dice(sides, once_count);
+        printf("sides: %d; count: %d; total: %d\n", sides, once_count, total);
+        printf("enter dice_sides\n");
+    }
+    printf("roll_dice: %d\n", roll_count);
+}
---
+++ c primer plus/12.存储类别/掷骰子/骰子.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "head.h"
+
+int roll_count = 0;
+
+static int dice(int sides)
+{
+    srand((unsigned int)time(0));
+    int result = rand() % sides + 1;
+    roll_count++;
+    return result;
+}
+
+int roll_dice(int sides, int dice_count)
+{
+    int total = 0;
+    for (int d = 0; d < dice_count; d++)
+    {
+        total += dice(sides);
+    }
+    return total;
+}
